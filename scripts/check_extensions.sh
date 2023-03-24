a6c74f017bb38a41f5b9d6b61c771eecadf68362
Junior L. Botelho (JLB) <junowerther@gmail.com>
2023-03-24 23:02:23 +0000

    feat: create the check_extensions.sh to validate all extensions

---
+++ scripts/check_extensions.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# List of extensions to validate
+extensions="
+  iconv
+  mbstring
+  curl
+  openssl
+  tokenizer
+  xmlrpc
+  soap
+  ctype
+  zip
+  gd
+  simplexml
+  dom
+  xml
+  intl
+  json
+  pgsql
+  mysqli
+  pdo_pgsql
+  pdo_mysql
+  ldap
+  sockets
+"
+
+# Loop through the extensions and validate if they are enabled
+for ext in $extensions; do
+  if ! php -m | grep -i -q "^$ext$"; then
+    echo "[Error]: Extension $ext is not enabled"
+    exit 1
+  fi
+done
+
+echo "[OK] Extensions are enabled"
+exit 0
