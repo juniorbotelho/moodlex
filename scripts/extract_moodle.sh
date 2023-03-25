0998415e2741ba82ec322e98d5e87fa8d120d0d5
Junior L. Botelho (JLB) <junowerther@gmail.com>
2023-03-25 02:20:33 +0000

    refact: change execution approach of extract_moodle script

--- scripts/extract_moodle.sh
+++ scripts/extract_moodle.sh
@@ -2,6 +2,6 @@
 
 PATH="/var/www/html"
 
-tar -xvzf "$PATH/moodle-4.1.2.tgz"
-rm -rf "$PATH/moodle-*.tgz*"
-php7 moodle/install.php
+exec tar -xvzf "${PATH}/moodle-4.1.2.tgz" &&\
+     rm -rf "${PATH}/moodle-*.tgz*" &&
+     php7 "${PATH}/moodle/install.php"
