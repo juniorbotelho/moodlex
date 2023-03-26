48499e8b17433b4e0c8c7a2f09eb01d5ad4303fa
Junior L. Botelho (JLB) <junowerther@gmail.com>
2023-03-26 18:57:48 -0300

    refact: fix errors inside admin_installation script and change default envs

--- scripts/admin_installation.sh
+++ scripts/admin_installation.sh
@@ -3,28 +3,30 @@ export PATH="/bin:$PATH"
 export http_proxy=${HTTP_PROXY}
 export https_proxy=${HTTP_PROXY}
 
-sleep 10
-# See more: https://docs.moodle.org/401/en/Administration_via_command_line#Installation
-exec su-exec nginx php7 moodle/admin/cli/install.php \
-  --chmod=2770 \
-  --lang=${MOODLE_LANG:-"en"} \
-  --wwwroot=${MOODLE_WWW:-"http://127.0.0.1:8080"} \
-  --dataroot=${MOODLE_DATADIR:-"/var/www/moodledata"} \
-  --dbtype=${MOODLE_DB_TYPE:-"pgsql"} \
-  --dbhost=${MOODLE_DB_HOST:-"db"} \
-  --dbname=${MOODLE_DB_NAME:-"moodle"} \
-  --dbuser=${MOODLE_DB_USER:-"moodleuser"} \
-  --dbpass=${MOODLE_DB_PASSWORD:-"moodlepw"} \
-  --fullname=${MOODLE_SITE_FULLNAME:-"Moodle LMS"} \
-  --shortname=${MOODLE_SITE_SHORTNAME:-"Moodle"} \
-  --adminuser=${MOODLE_ADMIN_USER:-"admin"} \
-  --adminpass=${MOODLE_ADMIN_PASSWORD:-"admin"} \
-  --adminemail=${MOODLE_ADMIN_EMAIL:-"admin@localhost.local"} \
-  --supportemail=${MOODLE_SUPPORT_EMAIL:-"support@localhost.local"} \
-  --non-interactive \
-  --agree-license
+READY_FILE=$(cat moodle/config.php | grep -c "xsendfile = 'X-Accel-Redirect'")
 
-# finally, merge needed configurations inside the new config.php file
-if ! cat moodle/config.php | grep -i '$CFG->xsendfile'; then
-  su-exec nginx echo $(cat /tmp/config.php) > moodle/config.php
+if [ $READY_FILE -eq 0 ]; then
+sleep 10
+  # See more: https://docs.moodle.org/401/en/Administration_via_command_line#Installation
+  exec su-exec nginx php7 moodle/admin/cli/install.php \
+    --chmod=2770 \
+    --lang=${MOODLE_LANG:-"en"} \
+    --wwwroot=${MOODLE_WWW:-"http://127.0.0.1:8080"} \
+    --dataroot=${MOODLE_DATADIR:-"/var/www/moodledata"} \
+    --dbtype=${MOODLE_DB_TYPE:-"pgsql"} \
+    --dbhost=${MOODLE_DB_HOST:-"db"} \
+    --dbname=${MOODLE_DB_NAME:-"moodle"} \
+    --dbuser=${MOODLE_DB_USER:-"moodleuser"} \
+    --dbpass=${MOODLE_DB_PASSWORD:-"moodlepw"} \
+    --fullname=${MOODLE_SITE_FULLNAME:-"Moodle LMS"} \
+    --shortname=${MOODLE_SITE_SHORTNAME:-"Moodle"} \
+    --adminuser=${MOODLE_ADMIN_USER:-"admin"} \
+    --adminpass=${MOODLE_ADMIN_PASSWORD:-"admin"} \
+    --adminemail=${MOODLE_ADMIN_EMAIL:-"admin@localhost.local"} \
+    --supportemail=${MOODLE_SUPPORT_EMAIL:-"support@localhost.local"} \
+    --non-interactive \
+    --agree-license
+
+  # finally, merge needed configurations inside the new config.php file
+  su-exec nginx echo $(cat /tmp/config.php) >> moodle/config.php
 fi
