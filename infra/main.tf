45e711d88285d89bb3be751e4357eba4b911a70e
Junior L. Botelho (JLB) <junowerther@gmail.com>
2023-03-28 11:05:54 -0300

    chore: create initial terraform configuration

---
+++ infra/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+
+  required_version = ">= 1.4.2"
+}
+
+provider "aws" {
+  profile = "moodle"
+  region  = "us-east-1"
+}
