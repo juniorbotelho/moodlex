a14d5fc864e17e3e566eafd1f047c9c140daae1c
Junior L. Botelho (JLB) <junowerther@gmail.com>
2023-04-09 14:07:49 +0000

    refact: add random_admin_password variable

--- scripts/admin_installation.sh
+++ scripts/admin_installation.sh
@@ -5,10 +5,9 @@ export PATH="/bin:$PATH"
 export http_proxy=${HTTP_PROXY}
 export https_proxy=${HTTP_PROXY}
 
-# generate a random admin password
-random_admin_password = $(tr -dc A-Za-z0-9 </dev/urandom | head -c 16 ; echo '')
-
 if [ ! -f /moodle/config.php ]; then
+  # generate a random admin password
+  random_admin_password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   # See more: https://docs.moodle.org/401/en/Administration_via_command_line#Installation
   su-exec nginx php8 moodle/admin/cli/install.php \
     --lang=${MOODLE_LANG:-"en"} \
